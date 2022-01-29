0e92a437168b6c81ef431f843e29ec54c6b158bc
unbekanntunity <68324862+unbekanntunity@users.noreply.github.com>
2022-01-29 04:40:02 +0100

    Finished Wlan config app

--- esp32/wlan_config/main.py
+++ esp32/wlan_config/main.py
@@ -1,18 +1,31 @@
 import os
-os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+from misc.event_handling import EventHandler
 
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 from kivymd.app import MDApp
 
-from kivy.properties import StringProperty
-from kivymd.uix.behaviors import RoundedRectangularElevationBehavior
-from kivymd.uix.card import MDCard, MDCardSwipe
+from kivy.clock import Clock
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty
 
-from kivymd.uix.list import IRightBodyTouch, TwoLineAvatarIconListItem
+from kivy.uix.anchorlayout import AnchorLayout
+
+from kivymd.uix.list import IRightBodyTouch, TwoLineAvatarIconListItem, ILeftBodyTouch
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.snackbar import BaseSnackbar
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 
 from misc.configuration import Configuration
+
+import collections
+
+
+class CustomSnackbar(BaseSnackbar):
+    text = StringProperty(None)
+    icon = StringProperty(None)
+    font_size = NumericProperty("15sp")
 
 
 class ListItem(TwoLineAvatarIconListItem):
@@ -21,37 +34,320 @@ class ListItem(TwoLineAvatarIconListItem):
 
     def __init__(self, name, password, **kw):
         super(ListItem, self).__init__(**kw)
+        self.on_arrow_up = None
+        self.on_arrow_down = None
+
+        self.on_edit = None
+        self.on_delete = None
+
         self.network_name = name
         self.network_password = password
 
 
 class RightContainer(IRightBodyTouch, MDBoxLayout):
-    adaptive_width = True
+    pass
+
+
+class LeftContainer(ILeftBodyTouch, MDBoxLayout):
+    pass
+
+
+class NetworkArea(AnchorLayout):
+    title = StringProperty()
+
+    network_card = ObjectProperty()
+    name_field = ObjectProperty()
+    pass_field = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        self.on_discard = None
+        self.on_save = None
+
+        self._visible = False
+        super(NetworkArea, self).__init__(**kwargs)
+
+    def on_touch_down(self, touch):
+        # True -> Wir haben den Touch registriert und verarbeitet und wollen es nicht, dass
+        # dieser Touch zu den Elternobjekte weitergeleitet wird
+        if self._visible and self.network_card.collide_point(*touch.pos):
+            super(NetworkArea, self).on_touch_down(touch)
+            return True
+        elif self._visible:
+            return True
+
+    def set_visibility(self, visible):
+        if hasattr(self, 'saved_attrs'):
+            if visible:
+                self.height, self.size_hint_y, self.opacity, self.disabled = self.saved_attrs
+                del self.saved_attrs
+                self._visible = True
+        elif not visible:
+            self.saved_attrs = self.height, self.size_hint_y, self.opacity, self.disabled
+            self.height, self.size_hint_y, self.opacity, self.disabled = 0, None, 0, True
+            self._visible = False
 
 
 class NetworkRoot(MDScreen):
+    def __init__(self, **kw):
+        self.old_networks = []
+        self.current_edited_idx = -1
+
+        self.add_network_dialog = None
+        super(NetworkRoot, self).__init__(**kw)
+
     def on_kv_post(self, base_widget):
-        self.set_visibility_widget(self.ids.network_card)
+        self.ids.add_network_area.set_visibility(False)
+        self.ids.edit_network_area.set_visibility(False)
+
+        self.ids.add_network_area.on_save = self.save_network
+        self.ids.add_network_area.on_discard = lambda *arguments: \
+            self.discard_network(self.ids.add_network_area, *arguments)
+
+        self.ids.edit_network_area.on_save = self.save_edited_network
+        self.ids.edit_network_area.on_discard = lambda *arguments: \
+            self.discard_network(self.ids.edit_network_area, *arguments)
 
         networks = MDApp.get_running_app().configuration.config_dict['networks']
-        for network in networks:
-            self.ids.network_list.add_widget(ListItem(name=network['name'], password=network['password']))
+        self.add_to_network_list(networks)
 
-    def on_add(self, *args):
-        self.set_visibility_widget(self.ids.network_card, dohide=False)
+        self.old_networks = networks.copy()
 
-    def save_network(self, *args):
-        self.set_visibility_widget(self.ids.network_card)
+    def add_to_network_list(self, updated_part):
+        for network in updated_part:
+            entry = ListItem(name=network['name'], password=network['password'])
+            entry.on_edit = self.edit_network
+            entry.on_delete = self.delete_network
+            entry.on_arrow_up = lambda x: self.move_network('up', x)
+            entry.on_arrow_down = lambda y: self.move_network('down', y)
+            self.ids.network_list.add_widget(entry)
+
+    def add_network(self, *args):
+        self.ids.add_network_area.set_visibility(True)
+
+    def clear_networks(self, *args):
+        if self.add_network_dialog is None:
+            self.add_network_dialog = MDDialog(
+
+                text="Do u really want to delete all entries?",
+                buttons=[
+                    MDFlatButton(
+                        text="No",
+                        theme_text_color="Custom",
+                        text_color=MDApp.get_running_app().theme_cls.primary_color,
+                        on_release=self.cancel_clear
+                    ),
+                    MDFlatButton(
+                        text="Yes",
+                        theme_text_color="Custom",
+                        text_color=MDApp.get_running_app().theme_cls.primary_color,
+                        on_release=self.accept_clear
+                    )
+                ]
+            )
+            self.add_network_dialog.open()
+
+    def accept_clear(self, *args):
+        self.ids.network_list.clear_widgets()
+        self.add_network_dialog.dismiss(force=True)
+
+    def cancel_clear(self, *args):
+        self.add_network_dialog.dismiss(force=True)
+
+    def restore_networks(self, *args):
+        names = [child.network_name for child in self.ids.network_list.children]
+        passwords = [child.network_name for child in self.ids.network_list.children]
+
+        old_names = [network['name'] for network in self.old_networks]
+        old_passwords = [network['password'] for network in self.old_networks]
+
+        if collections.Counter(names) != collections.Counter(old_names) and\
+                collections.Counter(passwords) != collections.Counter(old_passwords):
+            self.ids.network_list.clear_widgets()
+
+            self.add_to_network_list(self.old_networks)
+            CustomSnackbar(
+                text='List refreshed!',
+                icon='information',
+                snackbar_x='10dp',
+                snackbar_y='10dp',
+                size_hint_x=.5
+            ).open()
 
     @staticmethod
-    def set_visibility_widget(widget, dohide=True):
-        if hasattr(widget, 'saved_attrs'):
-            if not dohide:
-                widget.height, widget.size_hint_y, widget.opacity, widget.disabled = widget.saved_attrs
-                del widget.saved_attrs
-        elif dohide:
-            widget.saved_attrs = widget.height, widget.size_hint_y, widget.opacity, widget.disabled
-            widget.height, widget.size_hint_y, widget.opacity, widget.disabled = 0, None, 0, True
+    def save_networks(*args):
+        try:
+            MDApp.get_running_app().configuration.save_config()
+            CustomSnackbar(
+                text='List saved!',
+                icon='information',
+                snackbar_x='10dp',
+                snackbar_y='10dp',
+                size_hint_x=.5
+            ).open()
+            Clock.schedule_once(lambda *arguments: exit(0), 1)
+        except Exception as e:
+            CustomSnackbar(
+                text='Something went wrong!',
+                icon='alert-circle-outline',
+                snackbar_x='10dp',
+                snackbar_y='10dp',
+                size_hint_x=.5
+            ).open()
+            pass
+
+    def save_network(self, *args):
+        networks = MDApp.get_running_app().configuration.config_dict['networks']
+
+        names = [network['name'] for network in networks]
+        passwords = [network['password'] for network in networks]
+
+        name_field = self.ids.add_network_area.name_field
+        password_field = self.ids.add_network_area.pass_field
+
+        if name_field.text == '':
+            CustomSnackbar(
+                text='Name cant be empty!',
+                icon='alert-circle-outline',
+                snackbar_x='10dp',
+                snackbar_y='10dp',
+                size_hint_x=.5
+            ).open()
+            return
+        if password_field.text == '':
+            CustomSnackbar(
+                text='Password cant be empty!',
+                icon='alert-circle-outline',
+                snackbar_x='10dp',
+                snackbar_y='10dp',
+                size_hint_x=.5
+            ).open()
+            return
+
+        if name_field.text in names and password_field.text in passwords:
+            CustomSnackbar(
+                text='Network already exists!',
+                icon='information',
+                snackbar_x='10dp',
+                snackbar_y='10dp',
+                size_hint_x=.5
+            ).open()
+            return
+        else:
+            new_network = {
+                    'name': name_field.text,
+                    'password': password_field.text
+                }
+
+            networks.append(new_network)
+
+            self.add_to_network_list([new_network])
+            self.ids.add_network_area.set_visibility(False)
+
+    @staticmethod
+    def discard_network(network_area, *args):
+        network_area.name_field.required = False
+        network_area.name_field.text = ''
+        network_area.name_field.required = True
+
+        network_area.pass_field.required = False
+        network_area.pass_field.text = ''
+        network_area.pass_field.required = True
+
+        network_area.set_visibility(False)
+
+    def move_network(self, direction, *args):
+        networks = MDApp.get_running_app().configuration.config_dict['networks']
+        items = self.ids.network_list.children.copy()
+
+        factor = {
+            'up': {
+                'value': 1,
+                'limit': len(items) - 1
+            },
+            'down': {
+                'value': -1,
+                'limit': 0
+            }
+        }
+
+        for i, child in enumerate(items):
+            if child == args[0]:
+                idx = len(items) - 1 - i
+                if i != factor[direction]['limit']:
+                    networks[idx], networks[idx - factor[direction]['value']] = networks[idx - factor[direction]['value']], networks[idx]
+                    self.ids.network_list.remove_widget(child)
+                    self.ids.network_list.add_widget(child, i + (1 * factor[direction]['value']))
+                    break
+                else:
+                    return
+
+    def edit_network(self, *args):
+        networks = MDApp.get_running_app().configuration.config_dict['networks']
+        for i, child in enumerate(self.ids.network_list.children):
+            if child == args[0]:
+                reversed_index = len(networks) - 1 - i
+                self.current_edited_idx = i
+                self.ids.edit_network_area.set_visibility(True)
+                self.ids.edit_network_area.name_field.text = networks[reversed_index]['name']
+                self.ids.edit_network_area.pass_field.text = networks[reversed_index]['password']
+                break
+
+    def save_edited_network(self, *args):
+        networks = MDApp.get_running_app().configuration.config_dict['networks']
+
+        item = self.ids.network_list.children[self.current_edited_idx]
+
+        names = [network['name'] for network in networks]
+        passwords = [network['password'] for network in networks]
+
+        name_field = self.ids.edit_network_area.name_field
+        password_field = self.ids.edit_network_area.pass_field
+
+        if name_field.text == '':
+            CustomSnackbar(
+                text='Name cant be empty!',
+                icon='alert-circle-outline',
+                snackbar_x='10dp',
+                snackbar_y='10dp',
+                size_hint_x=.5
+            ).open()
+            return
+        if password_field.text == '':
+            CustomSnackbar(
+                text='Password cant be empty!',
+                icon='alert-circle-outline',
+                snackbar_x='10dp',
+                snackbar_y='10dp',
+                size_hint_x=.5
+            ).open()
+            return
+
+        if name_field.text in names and password_field.text in passwords:
+            CustomSnackbar(
+                text='Network already exists!',
+                icon='information',
+                snackbar_x='10dp',
+                snackbar_y='10dp',
+                size_hint_x=.5
+            ).open()
+            return
+        else:
+            networks[len(networks) - 1 - self.current_edited_idx] = {
+                'name': name_field.text,
+                'password': password_field.text
+            }
+
+            item.network_name = name_field.text
+            item.network_pass = password_field.text
+            self.ids.edit_network_area.set_visibility(False)
+
+    def delete_network(self, *args):
+        networks = MDApp.get_running_app().configuration.config_dict['networks']
+        for i, child in enumerate(self.ids.network_list.children):
+            if child == args[0]:
+                networks.pop(i)
+                self.ids.network_list.remove_widget(args[0])
+                break
 
 
 class NetworkApp(MDApp):
